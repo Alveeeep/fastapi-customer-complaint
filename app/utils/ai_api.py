894f2b320b90dcc17feb079654da62e3478f7056
Alveeeep <alvir.misalimov@gmail.com>
2025-07-24 09:34:22 +0300

    updated ai api

--- app/utils/ai_api.py
+++ app/utils/ai_api.py
@@ -1,4 +1,3 @@
-from openai import DefaultAioHttpClient
 from openai import AsyncOpenAI
 from app.config import settings
 
@@ -6,7 +5,7 @@ from app.config import settings
 async def get_chatgpt_response(text: str) -> str:
     async with AsyncOpenAI(
             api_key=settings.OPENAI_TOKEN.get_secret_value(),
-            http_client=DefaultAioHttpClient(proxy=settings.PROXY_URL.get_secret_value()),
+            base_url="https://hubai.loe.gg/v1"
     ) as client:
         try:
             chat_completion = await client.chat.completions.create(
@@ -14,7 +13,7 @@ async def get_chatgpt_response(text: str) -> str:
                     {
                         "role": "user",
                         "content": f"Определи категорию жалобы: {text}."
-                                   f" Варианты: техническая, оплата, другое. Ответ только одним словом.",
+                                   f" Варианты: техническая, оплата, другое. Ответ только одним словом без знаков препинания",
                     }
                 ],
                 model="gpt-3.5-turbo",
